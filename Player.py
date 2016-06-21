69f9263cfdc48833f9823352c61c26d564b827cc
Luke Ferreira <Quantum@Lukes-MacBook-Air.local>
2016-06-21 09:36:02 -0400

    Added Classes and DugeonCrawler mode

--- Player.py
+++ Player.py
@@ -1,35 +1,57 @@
 from random import *
 
+
+#Player creates a template on which professions are built
+
 class Player:
+    #Base stats for player
     health = 8
+    maxHealth = 8
     attack = 0
     defense = 0
     initiative = 0
     initMod = 0
+    alive = True
+    #Skill Checks
+    #to be added soon
+
 
     def __init__(self, HP, ATK, DEF, INIT):
+        self.maxHealth = HP
         self.health = HP
         self.attack = ATK
         self.defense = DEF
         self.initMod = INIT
 
+    # rolls a D6 for damage
     def attackRoll(self):
         diceRoll = randint(1,6)
         atkRoll = self.attack + diceRoll
         return atkRoll
 
+    # rolls a D6 for initiative
     def rollInit(self):
         roll = randint(1,6) + self.initMod
         self.initiative = roll
+        return roll
 
+    # Causes player to lose health on attack
     def takeDamage(self, nmeATK):
         dmg = (nmeATK - self.defense)
         if dmg > 0:
             self.health = self.health - (nmeATK - self.defense)
+        if self.health <= 0:
+            self.alive = False
 
-    def castSpell(self, dmg):
-        # roll = randint(1,6)
-        # if roll > 3:
-        return dmg
-        # else:
-        # return 0
+    # what happens when the player deals damage to the enemy
+    def attackTurn(self, e):
+        hit = self.attackRoll()
+        e.takeDamage(hit)
+
+    def revive(self):
+        self.alive = True
+        self.health = self.maxHealth
+
+    #To be overriden by professsion classes
+    def specialAbility(self):
+        randint(1,6)
--- Professions.py
+++ Professions.py
@@ -1,11 +1,52 @@
 from Player import *
+from Enemy import *
+from random import *
+
+#The Professions file contains individual classes
+#Each class overrides or specifies the attributes of the player based on the core Player class
 
 class Barbarian(Player):
-    #something here
+    def attackTurn(self, e):
+        roll_1 = self.attackRoll()
+        roll_2 = self.attackRoll()
+        if(roll_1 == roll_2):
+            e.takeDamage(roll_1 + roll_2)
+        else:
+            e.takeDamage(roll_1)
+
+class Drunkard(Player):
+    attack = 1
+    defense = 1
+
+class Knight(Player):
+    attack = 1
+    defense = 1
 
 class Priest(Player):
+    spellCasts = 0          #tracks the number of times a spell is used
+
     def heal(self,target, amount):
         target.health += amount
+
+class Ranger(Player):
+    initMod = 3
+
+
+class Rogue(Player):
+    initMod = 1
+    def takeDamage(self, nmeATK):
+        mitigation = randint(1,6)
+        dmg = (nmeATK - self.defense)-mitigation
+        if dmg > 0:
+            self.health = self.health - dmg
+        if self.health <= 0:
+            self.alive = False
+
+class Scribe(Player):
+    def attackTurn(self, e):
+        elementalDmg = randint(1,2)
+        hit = self.attackRoll() + elementalDmg
+        e.takeDamage(hit)
 
 class Sorcerer(Player):
     def castSpell(self, target, dmg):
@@ -15,5 +56,3 @@ class Sorcerer(Player):
         # else:
         # return 0
 
-class Scribe(Player):
-    #imbued weapon
